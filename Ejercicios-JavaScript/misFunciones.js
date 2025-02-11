810f49a89d6cb16dd02866393ea8d88e327bfd85
CotyKazuf <2306594@ucc.edu.ar>
2025-02-11 20:28:08 -0300

    operaciones matematicas innerHTML

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -65,23 +65,23 @@ function calcularSuma(){
     var num1, num2;
     num1= Number(document.getElementsByName("sum_num1")[0].value);
     num2= document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0]. value = num1 + Number(num2);
+    document.getElementsByName("sum_total")[0]. innerHTML = num1 + Number(num2);
 }
 function calcularRes(){
     var numr1, numr2;
     numr1= Number(document.getElementsByName("res_num1")[0].value);
     numr2= document.getElementsByName("res_num2")[0].value;
-    document.getElementsByName("res_total")[0]. value = numr1 - Number(numr2);
+    document.getElementsByName("res_total")[0]. innerHTML = numr1 - Number(numr2);
 }
 function calcularMul(){
     var num1, num2;
     num1= Number(document.getElementsByName("mul_num1")[0].value);
     num2= document.getElementsByName("mul_num2")[0].value;
-    document.getElementsByName("mul_total")[0]. value = num1 * Number(num2);
+    document.getElementsByName("mul_total")[0]. innerHTML = num1 * Number(num2);
 }
 function calcularDiv(){
     var num1, num2;
     num1= Number(document.getElementsByName("div_num1")[0].value);
     num2= document.getElementsByName("div_num2")[0].value;
-    document.getElementsByName("div_total")[0]. value = num1 / Number(num2);
+    document.getElementsByName("div_total")[0]. innerHTML = num1 / Number(num2);
 }
