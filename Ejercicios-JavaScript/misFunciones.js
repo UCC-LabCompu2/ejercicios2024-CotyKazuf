eca2e29f5a05e444d9aff436733339131ba1c1ed
CotyKazuf <2306594@ucc.edu.ar>
2025-02-11 20:16:03 -0300

    me habia faltado algo

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -8,7 +8,10 @@
 function cambiarUnidades(id, valor){
     if(isNaN(valor)){
         alert("Se ingreso un valor incorrecto");
-        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_metro.value="";
+        document.lasUnidades.unid_pulgada.value="";
+        document.lasUnidades.unid_pie.value="";
+        document.lasUnidades.unid_yarda.value="";
     }else if (id=="metro"){
         document.lasUnidades.unid_pulgada.value=39.3701*valor;
         document.lasUnidades.unid_pie.value=3.28084*valor;
